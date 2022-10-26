9a4d42d5a065c47519701ef7ebffc544ff93ce17
TalatKhojikurbonov <khojikurbonovtalat@gmail.com>
2022-10-26 15:58:43 +0500

    new commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// let name = 'Djon'
+// let admin = name
+
+// console.log(admin);
+
+
+// let balance = 25000
+
+// // Траты
+// let car = 4480
+// let food = 890
+
+// // Доходы
+// let invoice = 5500
+// let stock = 4200
+
+// // Итоги
+// let totalBalance = 0
+// totalBalance = balance - (car + food) + (invoice + stock)
+// console.log(totalBalance);
+
+let a = 'aaa@bbb@ccc'
+console.log(a.replaceAll('@', '!'));
+
+let str = 'HELLOWORLD'
+
+str = str.toLowerCase()
+console.log(str.slice(0, 1).toUpperCase() + str.slice(1, 5)  + ' ' + str.slice(5));
+
+let rand = Number(String(Math.random()).slice(0, 1))
+console.log(rand);
